7c0665fd043b16257eafe3f47cbf1621d98a41e7
Nour <nourelden.0523066@gmail.com>
2026-08-01 21:22:11 +0300

    feat : Implement current user service

--- Azka Transaction Processing System/Azka Transaction Processing System.Application/Abstractions/Services/ICurrentUserService.cs
+++ Azka Transaction Processing System/Azka Transaction Processing System.Application/Abstractions/Services/ICurrentUserService.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Azka_Transaction_Processing_System.Application.Common.Interfaces
+namespace Azka_Transaction_Processing_System.Application.Abstractions.Services
 {
     public interface ICurrentUserService
     {
---
+++ Azka Transaction Processing System/Azka Transaction Processing System.Infrastructure/Security/CurrentUserService.cs
@@ -0,0 +1,54 @@
+﻿using Azka_Transaction_Processing_System.Application.Abstractions.Services;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azka_Transaction_Processing_System.Infrastructure.Security
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        private static readonly string[] UserIdClaimTypes =
+        {
+            ClaimTypes.NameIdentifier,
+            JwtRegisteredClaimNames.Sub,
+        };
+
+        public int UserId
+        {
+            get
+            {
+                var user = _httpContextAccessor.HttpContext?.User;
+
+                foreach (var claim in user?.Claims ?? Enumerable.Empty<Claim>())
+                {
+                    Console.WriteLine($"{claim.Type} = {claim.Value}");
+                }
+
+                foreach (var claimType in UserIdClaimTypes)
+                {
+                    var value = user?.FindFirst(claimType)?.Value;
+
+                    if (int.TryParse(value, out var id))
+                        return id;
+                }
+
+                throw new UnauthorizedAccessException("User ID claim not found.");
+            }
+        }
+
+        
+
+    }
+}
