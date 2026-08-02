7dad28578fa760648c4eff12ebc51798be092249
Nour <nourelden.0523066@gmail.com>
2026-08-02 22:00:20 +0300

    feat : get all branched Implementation V1

--- Azka Transaction Processing System/Azka Transaction Processing System.API/Controllers/BranchesController.cs
+++ Azka Transaction Processing System/Azka Transaction Processing System.API/Controllers/BranchesController.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Azka_Transaction_Processing_System.API.Commons;
+using Azka_Transaction_Processing_System.Application.Modules.Branches.GetAllBranches;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Azka_Transaction_Processing_System.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class BranchesController : ControllerBase
+    public class BranchesController : BaseApiController
     {
+        private readonly GetAllBranchesUseCase _getAllBranchesUseCase;
+
+        public BranchesController(GetAllBranchesUseCase getBranchesUseCase)
+        {
+            _getAllBranchesUseCase = getBranchesUseCase;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetBranches()
+        {
+            var branches = await _getAllBranchesUseCase.ExecuteAsync();
+            return OkResponse(branches);
+        }
+
     }
 }
