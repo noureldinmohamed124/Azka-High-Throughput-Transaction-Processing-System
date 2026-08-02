bbf820c81d4b8a45f4c5fed576350872454fd985
Nour <nourelden.0523066@gmail.com>
2026-08-02 21:02:22 +0300

    feat : get all payment methods implementation V1

---
+++ Azka Transaction Processing System/Azka Transaction Processing System.Application/Modules/PaymentMethods/GetPaymentMethods/GetPaymentMethodsUseCase.cs
@@ -0,0 +1,24 @@
+﻿using Azka_Transaction_Processing_System.Application.Abstractions.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azka_Transaction_Processing_System.Application.Modules.PaymentMethods.GetPaymentMethods
+{
+    public class GetPaymentMethodsUseCase
+    {
+        private readonly IPaymentMethodRepo _paymentMethodRepo;
+
+        public GetPaymentMethodsUseCase(IPaymentMethodRepo paymentMethodRepo)
+        {
+            _paymentMethodRepo = paymentMethodRepo;
+        }
+
+        public async Task<IReadOnlyList<PaymentMethodResponse>> ExecuteAsync()
+        {
+            return await _paymentMethodRepo.GetAllPaymentMethodsAsync();
+        }
+    }
+}
---
+++ Azka Transaction Processing System/Azka Transaction Processing System.Application/Modules/PaymentMethods/GetPaymentMethods/PaymentMethodResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azka_Transaction_Processing_System.Application.Modules.PaymentMethods.GetPaymentMethods
+{
+    public class PaymentMethodResponse
+    {
+        public int Id { get; init; }
+        public string Name { get; init; } = string.Empty;
+    }
+}
